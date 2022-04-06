6ddc243a938cbe46a68abdbd91bdde2dadaa98b7
Guilherme <guilhermescod@alu.ufc.br>
2022-04-06 11:35:10 -0300

    exercicios arcade

--- F_ARCADE/cod.hs
+++ F_ARCADE/cod.hs
@@ -2,6 +2,7 @@ at :: [a] -> Int -> a
 at xs 0 = head xs
 at (x : xs) ind = if ind < 0 then at xs (ind + length (xs)) else at xs (ind - 1)
 
+{- teste -}
 main :: IO ()
 main = do
     print $ at [2,7,3,9] 2  --3
